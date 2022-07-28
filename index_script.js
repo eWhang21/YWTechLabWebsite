8b87728fa35fec45f94f69aad44c922872ca50b4
Esther Whang <estherwhang@MacBook-Pro-2.local>
2022-07-28 17:43:00 -0500

    more updates including team memeber info, press, etc.

---
+++ index_script.js
@@ -0,0 +1,230 @@
+/* Team Member's Bio Buttons */
+
+/* Mentor 1 */
+var mentor1 = document.getElementById("mentor1_ID");
+var mentor1_btn = document.getElementById("mentor1_btnID");
+
+mentor1_btn.onclick = function() {
+    mentor1.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    mentor1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == mentor1) {
+    mentor1.style.display = "none";
+  }
+}
+
+
+/* Mentor 2 */
+// click to see member info
+var mentor2 = document.getElementById("mentor2_ID");
+var mentor2_btn = document.getElementById("mentor2_btnID");
+mentor2_btn.onclick = function() {
+    mentor2.style.display = "block";
+}
+
+// close member info
+var span2 = document.getElementsByClassName("close2")[0];
+span2.onclick = function() {
+    mentor2.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == mentor2) {
+    mentor2.style.display = "none";
+  }
+}
+
+/* Mentor 3 */
+// click to see member info
+var mentor3 = document.getElementById("mentor3_ID");
+var mentor3_btn = document.getElementById("mentor3_btnID");
+mentor3_btn.onclick = function() {
+    mentor3.style.display = "block";
+}
+
+// close member info
+var span3 = document.getElementsByClassName("close3")[0];
+span3.onclick = function() {
+    mentor3.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == mentor3) {
+    mentor3.style.display = "none";
+  }
+}
+
+///////////* Team Members *///////////
+
+/* Member 1 */
+var member1 = document.getElementById("member1_ID");
+var member1_btn = document.getElementById("member1_btnID");
+
+member1_btn.onclick = function() {
+    member1.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span3 = document.getElementsByClassName("close3")[0];
+
+// When the user clicks on <span> (x), close the modal
+span3.onclick = function() {
+    member1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member1) {
+    member1.style.display = "none";
+  }
+}
+
+/* Member 2 */
+var member2 = document.getElementById("member2_ID");
+var member2_btn = document.getElementById("member2_btnID");
+
+member2_btn.onclick = function() {
+    member2.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span4 = document.getElementsByClassName("close4")[0];
+
+// When the user clicks on <span> (x), close the modal
+span4.onclick = function() {
+    member2.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member2) {
+    member2.style.display = "none";
+  }
+}
+
+
+/* Member 3 */
+var member3 = document.getElementById("member3_ID");
+var member3_btn = document.getElementById("member3_btnID");
+
+member3_btn.onclick = function() {
+    member3.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span5 = document.getElementsByClassName("close5")[0];
+
+// When the user clicks on <span> (x), close the modal
+span5.onclick = function() {
+    member3.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member3) {
+    member3.style.display = "none";
+  }
+}
+
+
+/* Member 4 */
+var member4 = document.getElementById("member4_ID");
+var member4_btn = document.getElementById("member4_btnID");
+
+member4_btn.onclick = function() {
+    member4.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span6 = document.getElementsByClassName("close6")[0];
+
+// When the user clicks on <span> (x), close the modal
+span6.onclick = function() {
+    member4.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member4) {
+    member4.style.display = "none";
+  }
+}
+
+
+/* Member 5 */
+var member5 = document.getElementById("member5_ID");
+var member5_btn = document.getElementById("member5_btnID");
+
+member5_btn.onclick = function() {
+    member5.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span7 = document.getElementsByClassName("close7")[0];
+
+// When the user clicks on <span> (x), close the modal
+span7.onclick = function() {
+    member5.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member5) {
+    member5.style.display = "none";
+  }
+}
+
+
+/* Member 6 */
+var member6 = document.getElementById("member6_ID");
+var member6_btn = document.getElementById("member6_btnID");
+
+member6_btn.onclick = function() {
+    member6.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span8 = document.getElementsByClassName("close8")[0];
+
+// When the user clicks on <span> (x), close the modal
+span8.onclick = function() {
+    member6.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member6) {
+    member6.style.display = "none";
+  }
+}
+
+/* Member 7 */
+var member7 = document.getElementById("member7_ID");
+var member7_btn = document.getElementById("member7_btnID");
+
+member7_btn.onclick = function() {
+    member7.style.display = "block";
+}
+
+// Get the <span> element that closes the modal
+var span9 = document.getElementsByClassName("close9")[0];
+
+// When the user clicks on <span> (x), close the modal
+span9.onclick = function() {
+    member7.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == member7) {
+    member7.style.display = "none";
+  }
+}
